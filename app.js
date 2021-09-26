5133ff3cb40ac693460e0ef1699243253361c4b9
RSoto3 <rsoto@utem.cl>
2021-09-25 23:51:42 -0300

    grafo de prueba

---
+++ app.js
@@ -0,0 +1,21 @@
+var nodes = new vis.DataSet([
+    { id: 1, label: "Node 1" },
+    { id: 2, label: "Node 2" },
+    { id: 3, label: "Node 3" },
+    { id: 4, label: "Node 4" },
+    { id: 5, label: "Node 5" }
+  ]);
+  var edges = new vis.DataSet([
+    { from: 1, to: 3 },
+    { from: 1, to: 2 },
+    { from: 2, to: 4 },
+    { from: 2, to: 5 },
+    { from: 3, to: 3 }
+  ]);
+  var contenedor = document.getElementById("grafo1");
+  var data = {
+    nodes: nodes,
+    edges: edges
+  };
+  var options = {};
+  var network = new vis.Network(contenedor, data, options); 
